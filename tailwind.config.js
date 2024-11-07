8e0976461f5ab7c58a6e076b4370c5cec49d5b67
emir <emirkartal00@gmail.com>
2024-11-07 22:53:59 +0300

    feat: :lipstick: add project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      primary: {
+        light: "#F2F9FF",
+        DEFAULT: "#007DFC",
+        dark: "#0060C0",
+      },
+      red: "#AF3436",
+      yellow: "#D1961F",
+      green: "#34AF6F",
+      gray: "#AEAFB1",
+    },
     extend: {},
   },
   plugins: [],
