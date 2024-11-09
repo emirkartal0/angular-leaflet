21981bc307391be1d68f5c9400b277b5adcf11c9
emir <emirkartal00@gmail.com>
2024-11-09 18:59:14 +0300

    added date time to sensor-card comp

--- src/app/components/sensor-card/sensor-card.component.ts
+++ src/app/components/sensor-card/sensor-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Sensor } from '../../shared/types/sensor.type';
 import { CommonModule } from '@angular/common';
 import { BaseDataService } from '../../shared/services/base-data.service';
+import { DateTimePipe } from '../../shared/pipes/date-time.pipe';
 
 @Component({
     selector: 'app-sensor-card',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, DateTimePipe],
     templateUrl: './sensor-card.component.html',
     styleUrl: './sensor-card.component.css',
 })
